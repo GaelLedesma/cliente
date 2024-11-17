b685c76c25eeeb31ac29991c97693b9ec4d9b03e
gael <ledesgael0@gmail.com>
2024-11-17 02:25:11 -0600

    prod

--- server.js
+++ server.js
@@ -11,16 +11,13 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
-// Ruta principal
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
